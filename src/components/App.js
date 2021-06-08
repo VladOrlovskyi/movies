dfd78da1021b7d2dba5add74c3d8b629d0bcdcdb
VladOrlovskyi <42379061+VladOrlovskyi@users.noreply.github.com>
2021-06-08 22:51:04 +0300

    v2.0

---
+++ src/components/App.js
@@ -0,0 +1,66 @@
+import "./App.css";
+import React from "react";
+import Filters from "./Filters/Filters";
+import MovieList from "./Movies/MovieList";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filters: { sort_by: "popularity.desc" },
+      page: 1
+
+    };
+  }
+
+  onChangeFilters = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState((prevState) => ({
+      filters: {
+        ...prevState.filters,
+        [name]: value,
+      },
+    }));
+  };
+
+
+  onChangePage = page => {
+    this.setState({
+      page
+    });
+  };
+
+  render() {
+    const { filters, page } = this.state;
+    return (
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-4">
+            <div className="card" style={{ width: "100%" }}>
+              <div className="card-body">
+                <h3>Фильтры:</h3>
+                <Filters
+                  page={page}
+                  filters={filters}
+                  onChangeFilters={this.onChangeFilters}
+                  onChangePage={this.onChangePage}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="col-8">
+            <MovieList
+              filters={filters}
+              page={page}
+              onChangePage={this.onChangePage}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
