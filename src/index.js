23878566dc205853465cfa83253b61b906c4f24f
VladOrlovskyi <42379061+VladOrlovskyi@users.noreply.github.com>
2021-08-11 14:17:52 +0300

    redux

--- src/index.js
+++ src/index.js
@@ -4,9 +4,40 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
+import { createStore } from 'redux';
+
+const actionCreaterUpdateAuth = (payload) => {
+  return {
+    type: "UPDATE_AUTH",
+    payload
+  }
+}
 
 
+const initialState = {
+  user: null,
+  session_id: null,
+}
+const reducerApp = (state = initialState, action) => {
+  switch (action.type) {
+    case "UPDATE_AUTH":
+      return {
+        ...state,
+        user: action.payload.user,
+        session_id: action.payload.session_id
+      };
+    default:
+      return state
+  }
+}
 
+const store = createStore(reducerApp)
+store.dispatch(actionCreaterUpdateAuth({
+  user: {
+    name: ""
+  },
+  session_id: ""
+}))
 ReactDOM.render(
 
   <App />
