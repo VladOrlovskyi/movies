0e0608c324b99d7206ed249d9e4a8b0c2a3c1bf8
VladOrlovskyi <42379061+VladOrlovskyi@users.noreply.github.com>
2021-08-31 22:44:16 +0300

    v2.3

---
+++ src/components/pages/MoviePage/MovieOverview.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Favorite from "../../Movies/Favorite";
+import WillWatch from "../../Movies/WillWatch";
+import Progressbar from "../../UIComponents/Progressbar";
+
+const MovieOverview = ({ title, overview, vote_average, movieId }) => {
+  return (
+    <React.Fragment>
+      <h1>{title}</h1>
+      <div>
+        <Progressbar vote_average={vote_average} />
+        <Favorite movieId={movieId} />
+        <WillWatch movieId={movieId} />
+      </div>
+      <p>{overview}</p>
+    </React.Fragment>
+  );
+};
+
+export default MovieOverview;
--- src/components/pages/MoviePage/MoviePage.jsx
+++ src/components/pages/MoviePage/MoviePage.jsx
@@ -1,13 +1,63 @@
 import React from "react";
 import CallApi from "../../../api/api";
+import MoviePageTabs from "../MoviePage/MoviePageTabs/MoviePageTabs";
+import Image from "../../UIComponents/Image";
+import MovieOverview from "./MovieOverview";
+import Loader from "../../UIComponents/Loader";
 
 export default class MoviePage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movieDetails: [],
+      isLoading: true,
+    };
+  }
+
   componentDidMount() {
-    CallApi.get(`/movie/${this.props.match.params.id}`);
+    const {
+      match: { params },
+    } = this.props;
+
+    CallApi.get(`/movie/${params.id}`, {
+      params: { language: "ru-RU" },
+    }).then((data) => {
+      this.setState({
+        movieDetails: data,
+        isLoading: false,
+      });
+    });
   }
 
   render() {
-    console.log(this.props);
-    return <div>MoviePage</div>;
+    const { movieDetails, isLoading } = this.state;
+    const imgUrl = `https://image.tmdb.org/t/p/w500${movieDetails.backdrop_path}`;
+    return isLoading ? (
+      <Loader />
+    ) : (
+      <React.Fragment>
+        <section
+          className="movie-overview"
+          style={{ backgroundImage: `url(${imgUrl})` }}
+        >
+          <div className="container">
+            <div className="row justify-content-center">
+              <div className="col-4">
+                <Image path={movieDetails.poster_path} width="100%" alt="" />
+              </div>
+              <div className="col-6">
+                <MovieOverview
+                  title={movieDetails.title}
+                  overview={movieDetails.overview}
+                  vote_average={movieDetails.vote_average}
+                  movieId={movieDetails.id}
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+        <MoviePageTabs movieDetails={movieDetails} />
+      </React.Fragment>
+    );
   }
 }
